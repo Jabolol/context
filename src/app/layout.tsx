63033d43e87fcd559a15bb706ddc787c1fec55c1
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-14 11:10:19 +0200

    [add](src/app/**) Add boilerplate landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Context',
-  description: 'Created for the Web3 Hackathon',
-}
+  title: "Context",
+  description: "Created for the Web3 Hackathon",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import About from "@/components/About";
+import Feature from "@/components/Features";
+import Footer from "@/components/Footer";
+import Header from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Pricing from "@/components/Pricing";
+import Quickstart from "@/components/Quickstart";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: "300",
+});
+
 export default function Home() {
   return (
-    <main className="flex items-center">
-      <h1>Hello world</h1>
+    <main className={`${inter.className}`}>
+      <Navbar />
+      <Header />
+      <Quickstart />
+      <Feature />
+      <About />
+      <Pricing />
+      <Footer />
     </main>
   );
 }
