a9e7f7cf819a1f8826bae77a8ba6f9f506db215b
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-15 12:13:29 +0200

    [add](src/components/Navbar.tsx) Add `connct` wallet option

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -67,29 +67,10 @@ export default function Nav() {
         </div>
         <ul className="flex items-center hidden space-x-8 lg:flex">
           <li>
-            <a
-              href="#sign-in"
-              aria-label="Sign in"
-              title="Sign in"
-              className="font-bold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-            >
-              Sign in
-            </a>
-          </li>
-          <li>
-            <a
-              href="#sign-up"
-              className="inline-flex items-center justify-center h-12 px-6 font-bold tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              aria-label="Sign up"
-              title="Sign up"
-            >
-              Sign up
-            </a>
-          </li>
-          <li>
-            <a>
-              <WalletSelectButton setIsConnected={() => setIsWalletConnected(true)}/>
-            </a>
+            <WalletSelectButton
+              setIsConnected={() => setIsWalletConnected(true)}
+              isMobile={false}
+            />
           </li>
         </ul>
         <div className="lg:hidden">
@@ -190,24 +171,10 @@ export default function Nav() {
                       </a>
                     </li>
                     <li>
-                      <a
-                        href="#sign-in"
-                        aria-label="Sign in"
-                        title="Sign in"
-                        className="font-bold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >
-                        Sign in
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#sign-up"
-                        className="inline-flex items-center justify-center w-full h-12 px-6 font-bold tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                        aria-label="Sign up"
-                        title="Sign up"
-                      >
-                        Sign up
-                      </a>
+                      <WalletSelectButton
+                        setIsConnected={() => setIsWalletConnected(true)}
+                        isMobile={true}
+                      />
                     </li>
                   </ul>
                 </nav>
